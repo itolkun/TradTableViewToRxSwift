a41cca7456fa11c6cb920e34189f326e9dd31e8b
 Aitolkun Anarbekova <tokusha.aa@gmail.com>
2024-01-10 17:55:28 +0600

    Initial Commit

---
+++ TradTableViewToRxSwift1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradTableViewToRxSwift1
+//
+//  Created by Айтолкун Анарбекова on 10.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
